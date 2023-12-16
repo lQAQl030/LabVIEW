b5b5aad9d88d818a0a03b5ccd8e68f718a08870f
Ricky <rickychouw@gmail.com>
2023-12-17 00:17:48 +0800

    main game development

--- test.c
+++ test.c
@@ -1,14 +1,66 @@
+// Bomb detection
 int i, j;
-int map[11][11];
 for(i = 0 ; i < 11 ; i++){
     for(j = 0 ; j < 11 ; j++){
-        map[i][j] = 0;
-    }
-}
+        if(map[i][j] == 5){
+            map[i][j] = 4;
+            //UP
+            if(i - 1 >= 0 && map[i - 1][j] < 5){
+                map[i-1][j] = 4;
+                if(i - 2 >= 0 && map[i - 2][j] < 5){
+                    map[i-2][j] = 4;
+                }else if(10 <= map[i - 2][j] && map[i - 2][j] <= 17){
+                    if(10 <= map[i - 2][j] && map[i - 2][j] <= 13) h1--;
+                    else h2--;
+                }
+            }else if(10 <= map[i - 1][j] && map[i - 1][j] <= 17){
+                if(10 <= map[i - 1][j] && map[i - 1][j] <= 13) h1--;
+                else h2--;
+            }
 
-for(i = 0 ; i < 11 ; i++){
-    map[0][i] = 9;
-    map[10][i] = 9;
-    map[i][0] = 9;
-    map[i][10] = 9;
+            //Left
+            if(j - 1 >= 0 && map[i][j - 1] < 5){
+                map[i][j-1] = 4;
+                if(j - 2 >= 0 && map[i][j - 2] < 5){
+                    map[i][j-2] = 4;
+                }else if(10 <= map[i][j - 2] && map[i][j - 2] <= 17){
+                    if(10 <= map[i][j - 2] && map[i][j - 2] <= 13) h1--;
+                    else h2--;
+                }
+            }else if(10 <= map[i][j - 1] && map[i][j - 1] <= 17){
+                if(10 <= map[i][j - 1] && map[i][j - 1] <= 13) h1--;
+                else h2--;
+            }
+
+            //Down
+            if(i + 1 < 11 && map[i + 1][j] < 5){
+                map[i+1][j] = 4;
+                if(i + 2 < 11 && map[i + 2][j] < 5){
+                    map[i+2][j] = 4;
+                }else if(10 <= map[i + 2][j] && map[i + 2][j] <= 17){
+                    if(10 <= map[i + 2][j] && map[i + 2][j] <= 13) h1--;
+                    else h2--;
+                }
+            }else if(10 <= map[i + 1][j] && map[i + 1][j] <= 17){
+                if(10 <= map[i + 1][j] && map[i + 1][j] <= 13) h1--;
+                else h2--;
+            }
+
+            //Right
+            if(j + 1 < 11 && map[i][j + 1] < 5){
+                map[i][j+1] = 4;
+                if(j + 2 < 11 && map[i][j + 2] < 5){
+                    map[i][j+2] = 4;
+                }else if(10 <= map[i][j + 2] && map[i][j + 2] <= 17){
+                    if(10 <= map[i][j + 2] && map[i][j + 2] <= 13) h1--;
+                    else h2--;
+                }
+            }else if(10 <= map[i][j + 1] && map[i][j + 1] <= 17){
+                if(10 <= map[i][j + 1] && map[i][j + 1] <= 13) h1--;
+                else h2--;
+            }
+        }else if(0 < map[i][j] && map[i][j] <= 8){
+            map[i][j]--;
+        }
+    }
 }
